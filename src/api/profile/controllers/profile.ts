9edd177aefe3c5e114d87707d1a2a58de76f4978
reyraa <ali@muzikie.com>
2024-07-16 22:17:46 +0200

    Modfify profiles.fineOne to respond to profiles/me

--- src/api/profile/controllers/profile.ts
+++ src/api/profile/controllers/profile.ts
@@ -4,4 +4,30 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::profile.profile');
+export default factories.createCoreController('api::profile.profile', ({ strapi }) => ({
+  async findOne(ctx) {
+    const user = ctx.state.user;
+    if (!user) {
+      return ctx.unauthorized();
+    }
+
+    const knex = strapi.db.connection;
+    const link = await knex('profiles_admin_user_links')
+        .where('user_id', user.id)
+        .first();
+
+    if (!link) {
+      return ctx.notFound('Profile link not found');
+    }
+
+    const profile = await strapi.db.query('api::profile.profile').findOne({
+      where: { id: link.profile_id },
+    })
+
+    if (!profile) {
+      return ctx.notFound('Profile not found');
+    }
+
+    return profile;
+  }
+}));
