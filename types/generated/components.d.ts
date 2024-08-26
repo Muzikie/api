f3ab6350d5e708efca294ff4af41b04a852f2969
reyraa <ali@muzikie.com>
2024-08-26 14:10:31 +0200

    feat:add support for projects collections

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,30 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ContributionTierContributionTier extends Schema.Component {
+  collectionName: 'components_contribution_tier_contribution_tiers';
+  info: {
+    displayName: 'ContributionTier';
+    icon: 'chartPie';
+  };
+  attributes: {
+    title: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 3;
+      }>;
+    description: Attribute.Text &
+      Attribute.SetMinMaxLength<{
+        minLength: 20;
+      }>;
+    amount: Attribute.Integer & Attribute.Required;
+    rewards: Attribute.Text & Attribute.Required;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'contribution-tier.contribution-tier': ContributionTierContributionTier;
+    }
+  }
 }
