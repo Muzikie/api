d5da862860d1f8ca38d75f0383ff1647195a688a
reyraa <ali@muzikie.com>
2024-08-27 16:18:25 +0200

    refactor:abstract out the upload logic

--- src/api/project/controllers/project.ts
+++ src/api/project/controllers/project.ts
@@ -2,7 +2,7 @@
  * project controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 import { parseMultipartData } from '@strapi/utils';
 import {
   TOTAL_PROJECT_IMAGES,
@@ -11,91 +11,93 @@ import {
 } from '../../../constants/limits';
 import { convertByteToBit } from '../../../utils/file';
 
-export default factories.createCoreController('api::project.project', ({ strapi }) => ({
-  async update(ctx) {
-    const { id } = ctx.params;
-    const user = ctx.state.user;
+export default factories.createCoreController('api::project.project', ({ strapi }) => {
 
-    // Fetch the project
-    const project = await strapi.entityService.findOne('api::project.project', id, {
-      populate: {
-        images: true,
-        video: true,
-        audio: true,
+  // Helper function to upload files
+  const uploadFiles = async (files, id, ref, field, maxSize, allowedFileTypes) => {
+    if (!files) return [];
+
+    const newFiles = Array.isArray(files) ? files : [files];
+
+    // Check the size of each file
+    for (const file of newFiles) {
+      if (file.size > convertByteToBit(maxSize)) {
+        throw new Error(`Each ${allowedFileTypes} file must be smaller than ${maxSize} MB. The file "${file.name}" is too large.`);
       }
+    }
+
+    const uploadedFiles = await strapi.plugins['upload'].services.upload.upload({
+      data: { refId: id, ref: ref, field: field },
+      files: newFiles,
     });
 
-    // Ensure ownership
-    if (!project || project.id !== user.id) {
-      return ctx.unauthorized('Only the owner is allowed to update a project');
-    }
+    return uploadedFiles.map(file => file.id);
+  };
 
-    // Ensure the number and size of images are limited
-    const currentPhotoCount = project.images ? project.images.length : 0;
+  return {
+    async update(ctx) {
+      const { id } = ctx.params;
+      const user = ctx.state.user;
 
-    let entity;
-    if (ctx.is('multipart')) {
-      const { data, files } = parseMultipartData(ctx);
-      // Handle images
-      if (files.images) {
-        const newImages = Array.isArray(files.images) ? files.images : [files.images];
-        const newImagesCount = newImages.length;
-        const totalImages = currentPhotoCount + newImagesCount;
-
-        // Check if the total number of photos exceeds the limit
-        if (totalImages > TOTAL_PROJECT_IMAGES) {
-          return ctx.badRequest(`You can only have a maximum of ${TOTAL_PROJECT_IMAGES} images per project.`);
+      // Fetch the project
+      const project = await strapi.entityService.findOne('api::project.project', id, {
+        populate: {
+          images: true,
+          video: true,
+          audio: true,
+          users_permissions_user: true,
         }
+      });
 
-        // Check the size of each photo
-        for (const image of newImages) {
-          if (image.size > convertByteToBit(MEX_PROJECT_IMAGE_SIZE)) {
-            return ctx.badRequest(`Each image must be smaller than ${MEX_PROJECT_IMAGE_SIZE} MB. The image "${image.name}" is too large.`);
+      // Ensure ownership
+      if (!project || project.users_permissions_user.id !== user.id) {
+        return ctx.unauthorized('Only the owner is allowed to update a project');
+      }
+
+      const currentPhotoCount = project.images ? project.images.length : 0;
+
+      let entity;
+      if (ctx.is('multipart')) {
+        const { data, files } = parseMultipartData(ctx);
+
+        try {
+          // Handle images
+          if (files.images) {
+            const newImagesCount = Array.isArray(files.images) ? files.images.length : 1;
+            const totalImages = currentPhotoCount + newImagesCount;
+
+            // Check if the total number of photos exceeds the limit
+            if (totalImages > TOTAL_PROJECT_IMAGES) {
+              return ctx.badRequest(`You can only have a maximum of ${TOTAL_PROJECT_IMAGES} images per project.`);
+            }
+
+            const uploadedImageIds = await uploadFiles(files.images, id, 'api::project.project', 'images', MEX_PROJECT_IMAGE_SIZE, 'image');
+            data.images = project.images.concat(uploadedImageIds);
           }
+
+          // Handle video
+          if (files.video) {
+            const uploadedVideoId = await uploadFiles(files.video, id, 'api::project.project', 'video', MEX_PROJECT_VIDEO_SIZE, 'video');
+            data.video = uploadedVideoId[0]; // Since it's a single video
+          }
+
+          // Handle audio
+          if (files.audio) {
+            const uploadedAudioId = await uploadFiles(files.audio, id, 'api::project.project', 'audio', MEX_PROJECT_VIDEO_SIZE, 'audio');
+            data.audio = uploadedAudioId[0]; // Since it's a single audio file
+          }
+
+          entity = await strapi.entityService.update("api::project.project", id, { data });
+        } catch (error) {
+          return ctx.badRequest(error.message);
         }
-
-        const uploadedPhotos = await strapi.plugins['upload'].services.upload.upload({
-          data: { ...data, refId: id, ref: 'api::project.project', field: 'images' },
-          files: newImages,
-        });
-
-        data.photos = project.images.concat(uploadedPhotos.map(file => file.id));
+      } else {
+        entity = await strapi.entityService.update("api::project.project", id, ctx.request.body);
       }
 
-      // Handle video
-      if (files.video) {
-        if (files.video.size > convertByteToBit(MEX_PROJECT_VIDEO_SIZE)) {
-          return ctx.badRequest(`The video must be smaller than ${MEX_PROJECT_VIDEO_SIZE} MB.`);
-        }
-
-        const video = await strapi.plugins['upload'].services.upload.upload({
-          data: { ...data, refId: id, ref: 'api::project.project', field: 'video' },
-          files: files.video,
-        });
-
-        data.video = video[0].id;
-      }
-
-      // Handle audio
-      if (files.audio) {
-        if (files.audio.size > convertByteToBit(MEX_PROJECT_VIDEO_SIZE)) {
-          return ctx.badRequest(`The audio must be smaller than ${MEX_PROJECT_VIDEO_SIZE} MB.`);
-        }
-
-        const audio = await strapi.plugins['upload'].services.upload.upload({
-          data: { ...data, refId: id, ref: 'api::project.project', field: 'audio' },
-          files: files.audio,
-        });
-
-        data.audio = audio[0].id;
-      }
-
-      entity = await strapi.entityService.update("api::project.project", id, { data });
-    } else {
-      entity = await strapi.entityService.update("api::project.project", id, ctx.request.body);
+      const sanitizedResults = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanitizedResults);
     }
+  };
+});
 
-    const sanitizedResults = await this.sanitizeOutput(entity, ctx);
-    return this.transformResponse(sanitizedResults);
-  }
-}));
