b3bf0dcd1aeb25d4136d78e709f99c2557187a42
reyraa <ali@muzikie.com>
2024-08-28 20:51:38 +0200

    fix:improve project creation controller syntax

--- src/api/project/controllers/project.ts
+++ src/api/project/controllers/project.ts
@@ -105,19 +105,20 @@ export default factories.createCoreController('api::project.project', ({ strapi 
       const { user } = ctx.state;
   
       try {
-        const project = await strapi.entityService.create(
-          'api::project.project',
-          {
-            data: {
-              user_id: user.id,
-              ...ctx.request.body,
-            },
-          });
+        const now = new Date();
+        ctx.request.body.data.users_permissions_user = user.id;
+        ctx.request.body.data.users_permissions_user = user.id;
+        ctx.request.body.data.createdAt = now;
+        ctx.request.body.data.updatedAt = now
+        ctx.request.body.data.publishedAt = now
   
+        // Proceed with creating the the project
+        const result = await super.create(ctx);
+
         // TODO: Call the Smart Contract method here to register the project on the blockchain
-  
-        const sanitizedEntity = await this.sanitizeOutput(project, ctx);
-        return this.transformResponse(sanitizedEntity);
+        // and if not created, revert the centralized project creation.
+
+        return result;
       } catch (err) {
         ctx.throw(500, err);
       }
