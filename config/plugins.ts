d6e17955e40a28d2ebecf4b8025f069105fb8164
reyraa <ali@muzikie.com>
2024-10-07 17:00:00 +0200

    Delete oauth shit

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,12 +2,6 @@ module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
       jwtSecret: env('ADMIN_JWT_SECRET'),
-      providers: {
-        google: {
-          clientId: env('GOOGLE_CLIENT_ID'),
-          redirectUri: 'muzikie://auth/callback',
-        },
-      },
     },
   },
   upload: {
