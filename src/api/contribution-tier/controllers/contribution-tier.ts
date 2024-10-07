503e99b97ef891fd68ce97852bfdfed7bc41264f
reyraa <ali@muzikie.com>
2024-10-07 14:43:44 +0200

    feat:add avatar image

--- src/api/contribution-tier/controllers/contribution-tier.ts
+++ src/api/contribution-tier/controllers/contribution-tier.ts
@@ -13,8 +13,8 @@ export default factories.createCoreController('api::contribution-tier.contributi
       const now = new Date();
       ctx.request.body.data.users_permissions_user = user.id;
       ctx.request.body.data.createdAt = now;
-      ctx.request.body.data.updatedAt = now
-      ctx.request.body.data.publishedAt = now
+      ctx.request.body.data.updatedAt = now;
+      ctx.request.body.data.publishedAt = now;
 
       // Proceed with creating the the project
       const result = await super.create(ctx);
