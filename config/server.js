60aa21396171ab8e4eb0d752044bb720876273c2
Ali Haghighatkhah <ali@muzikie.com>
2024-12-17 12:01:04 +0100

    fix:add dev logs

--- config/server.js
+++ config/server.js
@@ -1,11 +1,15 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env(URL, 'http://localhost:1337'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-});
+module.exports = ({ env }) => {
+  console.log('Check server envs', env('URL'), env('HOST'), env('PORT'));
+
+  return ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('URL', 'http://localhost:1337'),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+      populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+  });
+}
