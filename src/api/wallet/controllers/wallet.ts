998118fb0318aad7131eda00c5695dbdf70f6806
Ali Haghighatkhah <ali@muzikie.com>
2025-05-11 22:09:00 +0200

    Update wallet creation logic

--- src/api/wallet/controllers/wallet.ts
+++ src/api/wallet/controllers/wallet.ts
@@ -4,4 +4,30 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::wallet.wallet');
+export default factories.createCoreController(
+  'api::wallet.wallet',
+  ({ strapi }) => {
+    const walletDocs = strapi.documents('api::wallet.wallet');
+
+    return {
+      // POST
+      async create(ctx) {
+        const { user } = ctx.state;
+        try {
+          const { body: { data } } = ctx.request
+
+          const walletData = {
+            data: {
+              ...data,
+              users_permissions_user:  user.id,
+            },
+          };
+          return walletDocs.create(walletData)
+        } catch (err) {
+          ctx.throw(500, err);
+        }
+      },
+    };
+  },
+);
+
